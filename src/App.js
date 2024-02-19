bc16b57e4cc3bbab9a3a2565b404bf111e6e601b
Vailantan <vailantan@3003.com>
2024-02-19 22:22:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Your Google Sheets API endpoint
+    const sheetId = '795123173';
+    const range = 'Sheet1!D4:E4'; // Specify the range (row and column)
+
+    // Your API request
+    try {
+      await axios.post(
+        `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}`,
+        {
+          values: [[name]], // Data to be sent
+        },
+        {
+          params: {
+            key: 'AIzaSyAsxYoeup-Yh77VNfO1S-ECUmWUbRU0lsQ', // Your API key
+          },
+        }
+      );
+      console.log('Data sent successfully!');
+    } catch (error) {
+      console.error('Error sending data:', error);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+    <input
+      type="text"
+      placeholder="Name"
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+    />
+   
+    <button type="submit">Submit</button>
+  </form>
   );
 }
 
